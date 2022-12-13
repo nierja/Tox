fc59d7f1114763d5f82a2377e9e5b3c514b215bc
Jakub Nierostek <nierojak@fit.cvut.cz>
2022-12-13 14:32:00 +0100

    Create script to test programs for feature generation

---
+++ src/descriptor_generation/test_generation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+import signal
+import subprocess
+import time
+
+targets = ["NR-AR-LBD", "NR-Aromatase", "NR-ER", "NR-ER-LBD", "NR-PPAR-gamma", "SR-ARE", "SR-ATAD5", "SR-HSE", "SR-MMP", "SR-p53"]
+descriptors = [ "maccs" ]
+datasets = [ "training", "test", "eval" ]
+
+spacer = '------------------------------------------------------------------------'
+
+for descriptor in descriptors:
+    for target in targets:
+        for dataset in datasets:
+            print('\n', spacer)
+            print(f'Running "python3 generate_{dataset}_data.py --target={target}"\n')
+
+            pro = subprocess.Popen(
+                f'python3 generate_{dataset}_data.py --target={target} --fp={descriptor}', 
+                stdout=subprocess.PIPE, 
+                shell=True, 
+                preexec_fn=os.setsid
+            ) 
+            time.sleep(10)
+            os.killpg(os.getpgid(pro.pid), signal.SIGTERM) 
+
