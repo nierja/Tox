c28f7e2f041962a2f0cf787c0d4e88598aca5cd5
Jakub Nierostek <nierojak@fit.cvut.cz>
2023-01-03 12:54:01 +0100

    Make batch script more resilient

--- src/DL/hp_job.sh
+++ src/DL/hp_job.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 #PBS -N Keras_tuner_job
-#PBS -l select=1:ncpus=1:mem=16gb:scratch_local=32gb
-#PBS -l walltime=4:00:00
+#PBS -l select=1:ncpus=1:mem=16gb:scratch_local=32gb:cluster=halmir
 
 # define a DATADIR variable: directory where the input files are taken from and where output will be copied to
-DATADIR=/auto/brno2/home/nierja/Tox/src/DL # substitute username and path to to your real username and path
+DATADIR=/auto/brno2/home/nierja/Tox/src/DL
+LOGDIR=/auto/brno2/home/nierja/Tox/results/logs
+MODELDIR=/auto/brno2/home/nierja/Tox/results/models
 
 # append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of node it is run on and the path to a scratch directory
 # this information helps to find a scratch directory in case the job fails and you need to remove the scratch directory manually 
 echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $DATADIR/jobs_info.txt
 
-# '--use-pep517'
-
 cd $DATADIR
 export TMPDIR=$SCRATCHDIR
+
 module add python/3.8.0-gcc-rab6t cuda/cuda-11.2.0-intel-19.0.4-tn4edsz cudnn/cudnn-8.1.0.77-11.2-linux-x64-intel-19.0.4-wx22b5t
 python3 -m venv HP_SEARCH2
 HP_SEARCH2/bin/pip install --no-cache-dir --upgrade pip setuptools
 HP_SEARCH2/bin/pip install --no-cache-dir tensorflow==2.8.0 tensorflow-addons==0.16.1 tensorflow-probability==0.16.0 tensorflow-hub==0.12.0 scipy numpy pandas tabulate matplotlib rdkit talos mordred matplotlib molvs keras_tuner scikit-learn==1.1.2
 source ./HP_SEARCH2/bin/activate
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-alias ls="ls --color"
-export PS1="{\[\e[32m\]\u\[\e[m\]@\[\e[36m\]\h\[\e[m\]:\W_\$?}$ "
-
 
 # test if scratch directory is set
 # if scratch directory is not set, issue error message and exit
 test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }
 
-python Tox21_tuner.py --target=$target --fp=$fp --pca=$pca --weighted=$weight --ensamble=$ensamble
+# run the hyperparameter search, while logging into the $SCRATCHDIR
+python Tox21_tuner.py --target=$target --fp=$fp --pca=$pca --weighted=$weight --ensamble=$ensamble --log_dir=$SCRATCHDIR > /dev/null 2>&1
+
+# append the simulation results into the working directory
+cat $SCRATCHDIR/DL_$target.csv >> $LOGDIR/DL_$target.csv
+
+# store best models
+cp -r $SCRATCHDIR/ensemble_model $MODELDIR
+cp -r $SCRATCHDIR/best_single_model_* $MODELDIR
 
 # clean the SCRATCH directory
 clean_scratch
--- src/DL/hp_search_all.sh
+++ src/DL/hp_search_all.sh
@@ -1,21 +1,64 @@
 #!/bin/bash
 cd /auto/brno2/home/nierja/Tox/src/DL
-targets=("NR-AR") # "NR-AR-LBD" "NR-AhR" "NR-Aromatase" "NR-ER" "NR-ER-LBD" "NR-PPAR-gamma" "SR-ARE" "SR-ATAD5" "SR-HSE" "SR-MMP" "SR-p53")
-# descriptors=('ecfp4')
-descriptors=('ecfp4_maccs' 'maccs_rdk7' 'ecfp4_rdk7' 'ecfp4' 'ecfp6' 'fcfp4' 'fcfp6' 'maccs' 'hashap' 'hashtt' 'avalon' 'rdk7' 'eigenvals' 'rdkit_descr' 'CMat_600' 'mordred')
-# descriptors=('ecfp4_maccs' 'maccs_rdk7' 'ecfp4_rdk7' 'ecfp0' 'ecfp2' 'ecfp4' 'ecfp6' 'fcfp2' 'fcfp4' 'fcfp6' 'maccs' 'hashap' 'hashtt' 'avalon' 'rdk5' 'rdk6' 'rdk7' 'eigenvals' 'rdkit_descr' 'CMat_400' 'CMat_600' 'mordred')
-pcas=('0' '512') # '1024')
-weights=('False') # 'True')
-ensambles=('7') # '17' '37')
+targets=("NR-AR" "NR-AR-LBD" "NR-AhR" "NR-Aromatase" "NR-ER" "NR-ER-LBD" "NR-PPAR-gamma" "SR-ARE" "SR-ATAD5" "SR-HSE" "SR-MMP" "SR-p53")
+targets=("SR-p53")
+# descriptors=('mordred')
+# descriptors=('ecfp4_maccs' 'maccs_rdk7' 'ecfp4_rdk7' 'ecfp4' 'ecfp6' 'fcfp4' 'fcfp6' 'maccs' 'hashap' 'hashtt' 'avalon' 'rdk7' 'eigenvals' 'rdkit_descr')
+descriptors=('ecfp4_maccs' 'maccs_rdk7' 'ecfp4_rdk7' 'ecfp0' 'ecfp2' 'ecfp4' 'ecfp6' 'fcfp2' 'fcfp4' 'fcfp6' 'maccs' 'hashap' 'hashtt' 'avalon' 'rdk5' 'rdk6' 'rdk7' 'eigenvals' 'rdkit_descr' 'CMat_400' 'CMat_600' 'mordred')
+
+short=('maccs' 'eigenvals' 'rdkit_descr' 'CMat_400')
+
+
+i=1
+pcas=('0' '512' '1024')
+weights=('False' 'True')
+ensambles=('7') # '3' '11')
 
 for target in ${targets[@]}; do
 	for fp in ${descriptors[@]}; do
-        	for pca in ${pcas[@]}; do
+		for pca in ${pcas[@]}; do
 			for weight in ${weights[@]}; do
 				for ensamble in ${ensambles[@]}; do
-  		    			qsub -v fp=$fp,target=$target,pca=$pca,weight=$weight,ensamble=$ensamble hp_job.sh
+
+					# compute needed walltime
+					time=6
+					if [ "$target" = 'NR-Aromatase' ] || 
+					   [ "$target" = 'SR-MMP' ]; then
+						time=$(( time+4 ))
+					fi
+
+					if [ "$fp" = 'ecfp4_rdk7' ] || 
+					   [ "$fp" = 'mordred' ]; then
+						time=$(( 5*time ))
+					fi
+
+					if [ "$pca" = '1024' ] && 
+					   [ "$fp" != 'mordred' ]; then
+						continue
+					fi
+
+					too_short=0
+					for short_fingerprint in "${short[@]}"; do
+					    if [ $fp == "$short_fingerprint" ]; then
+							too_short=1
+						fi
+					done
+
+					if [ $too_short = "1" ] && 
+					   (( pca > 0 )); then
+						continue
+					fi
+
+					printf "job %s\t\t TARGET=%s\t FP=%s\t TIME=%s:00:00 ( %s, %s, %s )\n" $i $target $fp $time $pca $weight $ensamble
+
+					qsub -l walltime=$time:00:00 -v fp=$fp,target=$target,pca=$pca,weight=$weight,ensamble=$ensamble hp_job.sh
+
+					printf "\n"
+					i=$(( i+1 ))
 				done
 			done
-        	done
+		done
 	done
 done
+
+exit 0
