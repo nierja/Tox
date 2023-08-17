41edd4b2f079068aecf8331f571061a9a4cc42c0
Jakub Nierostek <nierojak@fit.cvut.cz>
2023-08-17 14:35:04 +0200

    Fix imports

--- lib/initialize_venv.sh
+++ lib/initialize_venv.sh
@@ -3,5 +3,5 @@
 cd lib
 python3 -m venv TOX_GPU_VENV
 TOX_GPU_VENV/bin/pip install --no-cache-dir --upgrade pip setuptools
-TOX_GPU_VENV/bin/pip install --no-cache-dir tensorflow==2.8.0 tensorflow-addons==0.16.1 tensorflow-probability==0.16.0 tensorflow-hub==0.12.0 scipy numpy scikit-learn pandas tabulate matplotlib rdkit mordred matplotlib molvs
+TOX_GPU_VENV/bin/pip install --no-cache-dir tensorflow==2.8.0 tensorflow-addons==0.16.1 tensorflow-probability==0.16.0 tensorflow-hub==0.12.0 scipy numpy scikit-learn pandas tabulate matplotlib rdkit mordred matplotlib molvs keras_tuner
 cd ..
